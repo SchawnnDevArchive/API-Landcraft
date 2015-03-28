809b392c381cdb45d8532486caf2672fcda0a454
SchawnnDev <iperskill@gmail.com>
2015-03-28 12:55:28 +0100

    Dernier commit de la journée D:

--- src/main/java/fr/schawnndev/landcraft/API.java
+++ src/main/java/fr/schawnndev/landcraft/API.java
@@ -1,5 +1,11 @@
 package fr.schawnndev.landcraft;
 
+import fr.schawnndev.landcraft.datas.SQL;
+
 public class API {
+
+	public static SQL getSQL(){
+		return Main.getSql(); // Ne sert à rien ^^
+	}
 
 }
